06c3dd71f384ead746e05d9defebbf839936d5be
ambroiseodt <ambroiseodonnattechnologie@gmail.com>
2026-09-10 09:05:40 +0000

    Update script device

--- apps/ablation/scripts/gpt2.sh
+++ apps/ablation/scripts/gpt2.sh
@@ -14,7 +14,7 @@ model_size="base"
 seq_len=128
 batch_size=512
 n_steps=25
-device="cuda:2"
+device="cuda:0"
 session="gpt2"
 tmux new-session -d -s ${session}
 command="python -m apps.ablation.gpt2 run --model_size ${model_size} --dataset_name ${dataset_name} --batch_size ${batch_size} --seq_len ${seq_len} --n_steps ${n_steps} --device ${device}"
